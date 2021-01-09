46c97884144dce4ac57b5e2b14ab1f801f83f234
yashpatel5400 <ypatel5400@gmail.com>
2021-01-09 12:48:55 -0500

    Added soln

--- python_solutions/12.py
+++ python_solutions/12.py
@@ -1,39 +1,42 @@
+import math
+
 with open("input_12.txt", "r") as f:
     lines = f.readlines()
 
 cardinal = ["E", "N", "W", "S"]
 facing = 0
 
-pos = [0, 0]
+ship_pos = [0, 0]
+waypoint_pos = [10, 1]
 
 def move(direction, amount):
     if direction == "E":
-        pos[0] += amount
+        waypoint_pos[0] += amount
     elif direction == "W":
-        pos[0] -= amount
+        waypoint_pos[0] -= amount
     elif direction == "N":
-        pos[1] += amount
+        waypoint_pos[1] += amount
     elif direction == "S":
-        pos[1] -= amount
+        waypoint_pos[1] -= amount
 
 for line in lines:
     cleaned = line.strip()
     direction = cleaned[0]
     amount = int(cleaned[1:])
 
-    move_direction = None
     if direction == "F":
-        move_direction = cardinal[facing]
-    elif direction == "L":
-        turn = amount // 90
-        facing = (facing + turn) % len(cardinal)
-    elif direction == "R":
-        turn = amount // 90
-        facing = (facing - turn) % len(cardinal)
+        for _ in range(amount):
+            ship_pos[0] += waypoint_pos[0]
+            ship_pos[1] += waypoint_pos[1]
+    elif direction in ["L", "R"]:
+        angle = amount
+        if direction == "R":
+            angle *= -1
+        new_waypoint = [0, 0]
+        new_waypoint[0] = round(math.cos(angle / 180 * math.pi) * waypoint_pos[0] - math.sin(angle / 180 * math.pi) * waypoint_pos[1])
+        new_waypoint[1] = round(math.sin(angle / 180 * math.pi) * waypoint_pos[0] + math.cos(angle / 180 * math.pi) * waypoint_pos[1])
+        waypoint_pos = new_waypoint
     else:
-        move_direction = direction
+        move(direction, amount)
 
-    if direction != "R":
-        move(move_direction, amount)
-
-print(abs(pos[0]) + abs(pos[1]))
+print(abs(ship_pos[0]) + abs(ship_pos[1]))
